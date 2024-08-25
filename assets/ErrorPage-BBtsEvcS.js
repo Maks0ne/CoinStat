8133d100c451dcf88474423b75ba6e2168e2f339
Maksim Chystiakov <maks.goodluck23@gmail.com>
2024-08-25 13:02:01 +0200

    Updates

--- assets/ErrorPage-BBtsEvcS.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BnT9f5Ni.js";const s=()=>r.jsx("div",{className:"error-page",children:r.jsx("p",{children:"Error"})});export{s as E};
---
+++ assets/ErrorPage-CufV6S1q.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-v2tMGB11.js";const s=()=>r.jsx("div",{className:"error-page",children:r.jsx("p",{children:"Error"})});export{s as E};
